c7b3a88127340c061251472babd4ecdd9a703ee5
urauc <ursula.rauch@stud.fh-campuswien.ac.at>
2020-12-17 14:56:07 +0100

    Initial commit

---
+++ src/main/java/at/turtles/Main.java
@@ -0,0 +1,21 @@
+package at.turtles;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        //launch(args);
+
+
+
+        Platform.exit();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
