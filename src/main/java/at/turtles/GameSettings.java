e9b31ae5201888c4287c34170c6d11c636534ff9
Lukas Putz <lukas@putz.me>
2020-12-28 10:47:01 +0100

    Add GameSettings for static variables

---
+++ src/main/java/at/turtles/GameSettings.java
@@ -0,0 +1,8 @@
+package at.turtles;
+
+public class GameSettings {
+    public static String listPathOfChoice;
+    public static char[] wordToGuess;
+    public static boolean won;
+    public static String chosenAnimal;
+}
