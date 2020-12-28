14efb42ea53b4e6c8bda3defccfba0ab8d6c47b8
Lukas Putz <lukas@putz.me>
2020-12-28 10:46:00 +0100

    Add back button to Game Window

--- src/main/java/at/turtles/GameController.java
+++ src/main/java/at/turtles/GameController.java
@@ -2,10 +2,15 @@ package at.turtles;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -50,5 +55,18 @@ public class GameController implements Initializable {
             }
         }
         updateLabels();
+    }
+
+    public void backButtonClicked(ActionEvent actionEvent) {
+        try {
+            Stage stage = (Stage) wordLabel.getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getResource("/chooseList.fxml"));
+            Scene scene = new Scene(root, 500, 500);
+            stage.setTitle("Save the Animals");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
