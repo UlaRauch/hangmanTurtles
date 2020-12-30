6a075b993d544bd7a371544aa64568a935293e4c
Lukas Putz <lukas@putz.me>
2020-12-30 15:35:27 +0100

    Add end game functionality in finish window

--- src/main/java/at/turtles/Finish.java
+++ src/main/java/at/turtles/Finish.java
@@ -1,5 +1,6 @@
 package at.turtles;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -55,6 +56,6 @@ public class Finish {
 
     public void endGame(ActionEvent actionEvent) {
         //go back to start.fxml
-
+        Platform.exit();
     }
 }
