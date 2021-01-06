d1ac8f3fa4ec7eb28725a1fea61daf0a913f8f5f
Lukas Putz <lukas@putz.me>
2021-01-06 17:11:16 +0100

    Add Javadoc comments to Hangman.java Remove unused variables

--- src/main/java/at/turtles/StartController.java
+++ src/main/java/at/turtles/StartController.java
@@ -8,26 +8,28 @@ import javafx.scene.control.Label;
 
 import java.io.IOException;
 
+/**
+ * Controller for start.fxml
+ */
 public class StartController {
 
     public final static int WIDTH = 1000;
     public final static int HEIGHT = 500;
     public final static String WINDOWTITLE = "Start";
 
-    @FXML
-    private Label titleLabel;
-
-    @FXML
-    public Button startButton;
-
-    @FXML
-    private Button exitButton;
-
+    /**
+     * is executed when Start Button is clicked in GUI
+     * switches to Choose Fighters Window
+     */
     public void startButtonclicked(ActionEvent actionEvent) throws IOException {
         System.out.println("Clicked 'Start'");
         GameSettings.showWindow("/choosefighters.fxml", ChooseFighters.WIDTH, ChooseFighters.HEIGHT, ChooseFighters.WINDOWTITLE);
     }
 
+    /**
+     * is executed when Exit Button is clicked in GUI
+     * exits the program
+     */
     public void onExit(ActionEvent actionEvent) {
         System.out.println("Clicked exit");
         Platform.exit();
